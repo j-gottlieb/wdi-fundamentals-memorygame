be7f9a15d23b2c6e1e7fa7e12f50f2251fb4f0db
JESSE GOTTLIEB <jessegottlieb@JESSEs-MacBook-Pro.local>
2018-07-30 21:16:10 -0400

    finished game lofic and it works

--- js/main.js
+++ js/main.js
@@ -32,9 +32,11 @@ if (cardsInPlay[0] === cardsInPlay[1]) {
 		}
 }
 
-function flipCard(cardId) {
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
 if (cardsInPlay.length === 2) {
 	checkForMatch();
 	}
@@ -42,5 +44,14 @@ if (cardsInPlay.length === 2) {
 	console.log(cards[cardId].cardImage);
 };
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+	for(var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	};
+};
+
+createBoard();
